2f06878cf2307329cfb66de41967cea3af632ceb
MichelML <michmoreau.l@gmail.com>
2016-07-29 16:51:34 -0400

    implementing a build process for distribution

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+const gulp = require("gulp"),
+    concat = require('gulp-concat'),
+    minifyCss = require('gulp-minify-css'),
+    uglify = require('gulp-uglify'),
+    htmlmin = require('gulp-htmlmin'),
+    critical = require('critical').stream,
+    replace = require('replace'),
+    htmlreplace = require("gulp-html-replace");
+
+gulp.task('css-minify', function() {
+    return gulp.src(["./src/public/bower_components/materialize/dist/css/materialize.min.css",
+            "src/public/css/styles.css"
+        ])
+        .pipe(concat('all.min.css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('./dist/public/css'));
+});
+
+gulp.task('js-minify', function() {
+    return gulp.src(["./src/public/bower_components/localforage/dist/localforage.min.js",
+            "./src/public/bower_components/knockout/dist/knockout.js",
+            "./src/public/bower_components/jquery-2.2.4.min/index.js",
+            "./src/public/bower_components/materialize/dist/js/materialize.min.js",
+            "./src/public/js/main.js"
+        ])
+        .pipe(concat("all.min.js"))
+        .pipe(uglify())
+        .pipe(gulp.dest("./dist/public/js"));
+});
+
+gulp.task("html-minify", ['css-minify', 'js-minify'], function() {
+    return gulp.src(['./src/public/index.html'])
+        .pipe(htmlreplace({
+            'cssfiles': 'css/all.min.css',
+            'jsfiles': {
+                src: [['js/all.min.js']],
+                tpl: '<script src="%s"></script>'
+            }
+        }))
+        .pipe(gulp.dest('./dist/public'));
+});
+
+gulp.task("indexjs", function() {
+    return gulp.src(['./src/index.js'])
+        .pipe(gulp.dest('./dist'));
+});
+
+gulp.task("img", function() {
+    return gulp.src(['./src/public/img/**'])
+        .pipe(gulp.dest('./dist/public/img'));
+});
+
+gulp.task("fav", function() {
+    return gulp.src(['./src/public/favicons/**'])
+        .pipe(gulp.dest('./dist/public/favicons'));
+});
+
+
+gulp.task('default', ['css-minify', 'js-minify', 'html-minify', 'indexjs','img', 'fav']);
